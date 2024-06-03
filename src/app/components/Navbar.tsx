f86d8afa75c09e09b4fcd41644cf8dd2d1194f3d
Radifan <frutami19@gmail.com>
2024-06-03 11:18:54 +0700

    change Image component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 const Navbar = () => {
 	return (
 		<div className="flex justify-center">
 			<nav className="absolute flex rounded-full w-10/12 mt-5 p-4 bg-dark-primary items-center">
 				{/* Konten Navbar */}
-				<a href="/"><img className="ml-8 hover:animate-wiggle transition-all duration-300" style={{width: "126px"}} src="/asset/logo-light.png" alt="" /></a>
+				<a href="/"><Image className="ml-8 hover:animate-wiggle transition-all duration-300" style={{width: "126px"}} src="/asset/logo-light.png" alt="" /></a>
                 <ul className="flex ml-auto space-x-8 text-2xl mr-10 text-dark-invert font-bold">
                     <a className="hover:text-3xl hover:animate-wiggle transition-all duration-300"  href=""><li>About</li></a>
                     <a className="hover:text-3xl hover:animate-wiggle transition-all duration-300" href=""><li>Project</li></a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
     <div className="flex items-center justify-center min-h-dvh mt-5">
-    <img className="w-3/4" src="/asset/logo-dark.png" alt="landing page image" />
+    <Image className="w-3/4" src="/asset/logo-dark.png" alt="landing page image" />
     </div>
     </main>
   );
